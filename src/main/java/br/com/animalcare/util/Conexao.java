9ad3f9638dfe49e53d3860f44d3242e29a01c55c
Murilo0300 <murilo0300@hotmail.com.br>
2023-05-29 20:38:04 -0300

    alterações

--- src/main/java/br/com/animalcare/util/Conexao.java
+++ src/main/java/br/com/animalcare/util/Conexao.java
@@ -6,12 +6,14 @@ import java.sql.SQLException;
 
 public class Conexao {
 	
-	private static final String URL = "jdbc:mysql://localhost:3306/db_animalcare";
+	private static final String URL = "jdbc:mysql://1/db_animalcare";
 	private static final String USER = "root";
-	private static final String PASSWWORD = "@rc1723Ma";
+	private static final String PASSWWORD = null;
 	
 	public Connection getConnection() {
 		try {
+			
+
 			Class.forName("com.mysql.cj.jdbc.Driver");
 			 
 			return DriverManager.getConnection(URL, USER, PASSWWORD);
